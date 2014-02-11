a7b592623aaeface2a44837e0aa9ad67d9a621ce
lbracken <levi.bracken@gmail.com>
2014-02-10 23:46:15 -0500

    Initial drop of map rendering

--- static/js/taxmap.js
+++ static/js/taxmap.js
@@ -7,34 +7,87 @@ var maxRegionsToShowByName = 5;
 
 var currData;
 
+
 // ****************************************************************************
 // *                                                                          *
-// *  Project Logic                                                           *
+// *  Map Logic                                                               *
 // *                                                                          *
 // ****************************************************************************
 
-function updateTaxmap() {
+function renderMap() {
 
-	var zip = $("#zip").val().trim();
-	var prgmCost = $("#prgm_cost").val().trim() * 1003000;
+	// The code heavily follows D3 examples provided by Mike Bostock.
+	//   * http://bl.ocks.org/mbostock/4060606
+	//   * http://bl.ocks.org/mbostock/4699541
+	//   * http://bl.ocks.org/mbostock/4707858
 
-	// TODO: Validate this data...
+	// TODO: This currently only works for country resolution results,
+	// need to also support country and state resolution responses.
 
-	$("#updateTaxmap").hide();
-	$("#updateTaxmapLoading").fadeIn();
+	// TODO: dynamically get these values based upon view area
+	var mapWidth = 700;	
+	var mapHeight = 500;
 
-	$.getJSON("determine_tax_map",
-		{"zip" : zip, "prgm_cost" : prgmCost},
-		onDetermineTaxMapSuccess).fail(
-		onDetermineTaxMapFailure);
+	// Define a projection for the map
+	var projection = d3.geo.albersUsa()
+		.translate([mapWidth/2, mapHeight/2])
+		.scale([1000]);
+
+	// Define path generator
+	var path = d3.geo.path()
+		.projection(projection);
+
+	// Create and attach an SVG element (the map)
+	$("#map").empty();
+	var map = d3.select("#map")
+		.append("svg")
+		.attr("width", mapWidth)
+		.attr("height", mapHeight);
+
+    // Asynchronously load the GeoJSON data from the server
+    d3.json("static/data/us-counties.json", function(us) {
+
+		// Bind data and create one path per GeoJSON feature
+		var counties = map.append("g")
+			.attr("class", "counties")
+			.selectAll("path")
+				.data(topojson.feature(us, us.objects.counties).features)
+			.enter().append("path")
+				.attr("d", path)
+				.style("fill", styleRegion);
+		
+		var states = map.append("path")
+			.datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+			.attr("class", "states")
+			.attr("d", path);
+
+		$("#mapContainer").fadeIn();
+	});
 }
 
-function onDetermineTaxMapSuccess(response) {
-	currData = response;
-	$("#updateTaxmap").show();
-	$("#updateTaxmapLoading").hide();
-	$("#content").fadeIn();
-	$("#showMoreDetails").show();
+
+function styleRegion(region) {
+	// If the region is specified in currData, then shade it in.
+
+	// Temp only!!!!
+	// TODO: Don't iterate over the entire result set. The response should return
+	// some data structure that makes it simple to quickly check for an id...
+	for (var ctr=0; ctr < currData.regions.length; ctr++) {
+		if (region.id == currData.regions[ctr]["_id"]) {
+			return "red";
+		}
+	}
+	return "#DDD";
+}
+
+
+// ****************************************************************************
+// *                                                                          *
+// *  Summary Logic                                                           *
+// *                                                                          *
+// ****************************************************************************
+
+function renderSummary() {
 
 	// Lots of string concatination in this function.  Looks like this is
 	// the fastet method.  See: http://jsperf.com/string-concatenation/14
@@ -83,14 +136,11 @@ function onDetermineTaxMapSuccess(response) {
 	summaryMessage += "PRGM NAME"; // TOOD: Change to currData.prgm_name
 	summaryMessage += "."	
 	$("#summaryMessage").html(summaryMessage);
+	
+	$("#showMoreDetails").show();
+	$("#summaryContainer").fadeIn(2000);
 }
 
-function onDetermineTaxMapFailure(jqxhr, textStatus, error) {
-	currData = null;
-	$("#updateTaxmap").show();
-	$("#updateTaxmapLoading").hide();
-	$("#serverErrorMsg").show();
-}
 
 function getRegionSpan(region) {
 
@@ -105,6 +155,7 @@ function getRegionSpan(region) {
 	return regionSpan;
 }
 
+
 function showMoreDetails() {
 
 	if (!currData) {
@@ -112,6 +163,50 @@ function showMoreDetails() {
 	}
 
 	$("#showMoreDetails").hide();
+}
+
+// ****************************************************************************
+// *                                                                          *
+// *  Project Logic                                                           *
+// *                                                                          *
+// ****************************************************************************
+
+function updateTaxmap() {
+
+	var zip = $("#zip").val().trim();
+	var prgmCost = $("#prgm_cost").val().trim() * 1003000;
+
+	// TODO: Validate this data...
+
+	$("#updateTaxmap").hide();
+	$("#updateTaxmapLoading").fadeIn();
+	$("#serverErrorMsg").hide();
+	$("#mapContainer").hide();
+	$("#summaryContainer").hide();
+
+	// Call the server to determine the tax map
+	$.getJSON("determine_tax_map",
+		{"zip" : zip, "prgm_cost" : prgmCost},
+		onDetermineTaxMapSuccess).fail(
+		onDetermineTaxMapFailure);
+}
+
+
+function onDetermineTaxMapSuccess(response) {
+	currData = response;
+	$("#updateTaxmap").show();
+	$("#updateTaxmapLoading").hide();	
+
+	renderMap();
+	renderSummary();
+}
+
+
+function onDetermineTaxMapFailure(jqxhr, textStatus, error) {
+	currData = null;
+	$("#updateTaxmap").show();
+	$("#updateTaxmapLoading").hide();
+	$("#serverErrorMsg").show();
 }
 
 // ****************************************************************************
@@ -124,6 +219,7 @@ function formatInteger(number) {
 	// From: http://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
     return parseInt(number).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+
 
 function abbreviateNumber(number) {
 
